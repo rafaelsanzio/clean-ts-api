a611bb79f673542bcef3183e16beb2d029f3a482
rafaelsanzio <rafaelsanzio27@gmail.com>
2021-03-13 11:00:51 -0300

    Implement log class
    Implement log class

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,10 +1,12 @@
+import { Controller } from '@presentation/protocols';
 import { DbAddAccount } from '@data/usecases/add-account/db-add-account';
 import { BcryptAdapter } from '@infra/criptografy/bcrypt-adapter';
 import { AccountMongoRepository } from '@infra/db/mongodb/account-repository/account';
 import { SignUpController } from '@presentation/controllers/signup/signup';
 import { EmailValidatorAdapter } from '@utils/email-validator-adapter';
+import { LogControllerDecorator } from '@main/decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const bcryptAdapter = new BcryptAdapter(salt);
@@ -16,5 +18,5 @@ export const makeSignUpController = (): SignUpController => {
     dbAddAccount,
   );
 
-  return signUpController;
+  return new LogControllerDecorator(signUpController);
 };
