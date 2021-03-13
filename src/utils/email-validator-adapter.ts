91dfbd639bcd73a3c8f5a6579aac5ca7154ab268
rafaelsanzio <rafaelsanzio27@gmail.com>
2021-03-13 09:29:22 -0300

    Arrangement on email validator adapter

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,6 +1,7 @@
 import validator from 'validator';
 
-import { EmailValidator } from 'presentation/protocols/email-validator';
+import { EmailValidator } from '@presentation/protocols/email-validator';
+import { DbAddAccount } from '@data/usecases/add-account/db-add-account';
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
