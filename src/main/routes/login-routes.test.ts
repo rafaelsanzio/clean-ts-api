ba8df32445f600e30b7de9e017c15c2f1c7a6b3c
rafaelsanzio <rafaelsanzio27@gmail.com>
2021-03-20 15:55:05 -0300

    refactor: rename signup routes to login routes
    refactor: rename signup routes to login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import app from '../config/app';
 
 import { MongoHelper } from '@infra/db/mongodb/helpers/mongo-helper';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(String(process.env.MONGO_URL));
   });
@@ -16,15 +16,18 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Rafael',
-        email: 'rafaelsanzio27@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(200);
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Rafael',
+          email: 'rafaelsanzio27@gmail.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
   });
 });
